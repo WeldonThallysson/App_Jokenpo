65e27d9223f614792b6563f886b24e0958ca8ac6
WeldonThallysson <65944963+WeldonThallysson@users.noreply.github.com>
2022-03-15 23:35:24 -0300

    Create topo.js

---
+++ topo.js
@@ -0,0 +1,16 @@
+
+import React,{ Component } from 'react';
+import {View,Image} from 'react-native';
+
+class topo extends Component{
+  render(){
+    return(
+      <View>
+        <Image source={require('../../imgs/jokenpo.png')}/>
+      </View>
+
+      );
+  }
+}
+
+export default topo;# App_Jokenpo
